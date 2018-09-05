915c34f196f2c475ca6d53e781561715be60ab70
Wei-Cheng Pan <legnaleurc@gmail.com>
2018-09-05 10:51:18 +0900

    let webpack transpile jsx
    Now we can actually write the rule for JSX.
    `babel-loader` is a loader for webpack to use babel to transpile JS or JSX
    files. Babel needs `@babel/preset-react` to transpile JSX. Again, babel will
    look `.babelrc` for settings.
    
    ```
    npm i --save-dev @babel/preset-react babel-loader
    ```

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,2 +1,15 @@
 export default {
+  entry: './src/js/index.jsx',
+  module: {
+    rules: [
+      // js, jsx
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
 };
