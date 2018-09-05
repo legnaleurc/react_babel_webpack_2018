c6366a89a40f1d3905cb525ef49f5db8eeed8b34
Wei-Cheng Pan <legnaleurc@gmail.com>
2018-09-05 10:28:28 +0900

    add source files and install react
    The file locations are not fixed, you can change it in the webpack config.
    
    `main_frame.jsx` is the main widget for demo.
    `index.jsx` will attach the widget to `index.html`.
    
    Indeed we need to install React library.
    
    ```
    npm i --save-dev react react-dom
    ```

---
+++ src/js/main_frame.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+class MainFrame extends React.Component {
+
+  constructor () {
+    super();
+  }
+
+  render () {
+    return (
+      <h1>Hello, world!</h1>
+    );
+  }
+
+}
+
+
+export default MainFrame;
